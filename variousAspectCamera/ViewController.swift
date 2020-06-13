8248e7602d5b57c347d06e5d6a22fc3ade8ecfa1
james26k <cb26.hayashi@gmail.com>
2020-06-13 13:20:54 +0900

    first commit

---
+++ variousAspectCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  variousAspectCamera
+//
+//  Created by Kohei Hayashi on 2020/06/13.
+//  Copyright © 2020 Kohei Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
